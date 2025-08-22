fb31406d5ccec9511deff3edc810a5e0f25d9d29
KajiyaManzou <kajiya.coffee@gmail.com>
2025-08-22 15:39:37 +0900

    日本語入力対応

--- ai_my_memo/lib/main.dart
+++ ai_my_memo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'providers/memo_provider.dart';
 import 'screens/memo_edit_screen.dart';
@@ -19,6 +20,15 @@ class MyMemoApp extends StatelessWidget {
       ],
       child: MaterialApp(
       title: 'ai-MyMemo',
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ja', 'JP'),
+        Locale('en', 'US'),
+      ],
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.blue,
